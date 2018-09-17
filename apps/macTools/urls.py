be7149cb13f6ef1bec7093872627fdbb55e2670f
Windy5 <bob23456@163.com>
2018-09-17 17:37:12 +0800

    9.17白
    1.新增工具模块url
    2.新增工具模块获取工具类别、获取工具列表、获取工具详情接口

--- apps/macTools/urls.py
+++ apps/macTools/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'tools'
 urlpatterns = [
-    # 所有类别
-    #path('cate', views.get_resources_cate),
-    # 获取素材列表
-   # path('list/<int:cate>', views.get_resources_list),
+    # 获取所有工具类别
+    path('cate', views.get_tools_cate),
+    # 通过cate获取工具列表
+    path('list/<int:cate>', views.get_tools_allList),
     # 获取素材详情
-    #path('details/<int:id>', views.get_resources_details),
+    path('details/<int:id>', views.get_tools_details),
     # 获取素材详情的下载链接
     #path('cloud/<int:id>', views.get_resources_cloud),
     # 访问量统计
--- apps/macTools/views.py
+++ apps/macTools/views.py
@@ -4,6 +4,7 @@ from django.core import serializers
 from django.db.models import Count
 import json
 
+
 # Create your views here.
 # 工具点击量
 def get_tools_looks(request, id):
@@ -22,10 +23,80 @@ def get_tools_looks(request, id):
 def get_tools_downloads(request, id):
     tools = models.Tools.objects.get(id=int(id))
     tools.download_num = tools.download_num + 1
-    models.tools.objects.filter(id=int(id)).update(download_num=tools.download_num)
+    models.Tools.objects.filter(id=int(id)).update(download_num=tools.download_num)
     # print(resources.looked_num)
     data = {
         "download_num": tools.download_num,
         "state": "ok"
     }
     return HttpResponse(json.dumps(data))
+
+
+# 获取所有工具类别
+def get_tools_cate(request):
+    tools_cate = models.Tools.CATEGORY_TYPE
+    dic_data = []
+    for cate in tools_cate:
+        dic_data.append({'num': cate[0], 'val': cate[-1]})
+    # print(dicData)
+    data = json.dumps(dic_data)
+    return HttpResponse(data)
+
+
+# 获取全部工具列表
+def get_tools_allList(request, cate):
+    if cate == 0 or cate == False:
+        tools_list = models.Tools.objects.order_by("-create_time")
+    else:
+        tools_list = models.Tools.objects.filter(cate=int(cate)).order_by("-create_time")
+
+    tools_data = []
+    tools_cate = models.Tools.CATEGORY_TYPE
+    for list in tools_list:
+        data = {
+            "toolsIcon": str(list.icon),
+            "toolsTitle": list.title,
+            "toolsIntroduction": list.introduction,
+            "toolsCate": tools_cate[list.cate - 1],
+            "toolsId": list.id,
+        }
+        tools_data.append(data)
+    if tools_data:
+        return HttpResponse(json.dumps(tools_data))
+    else:
+        err = {
+            "msg": "没有数据",
+            "state": "err"
+        }
+        return HttpResponse(json.dumps(err))
+
+
+# 获取工具详情
+def get_tools_details(request, id):
+    tools_details = models.Tools.objects.filter(id=int(id))
+
+    """
+    返回 title、icon、cate、tag、introduction、new_version、content、looked_num、download_num、create_time
+    """
+    details = []
+    tools_cate = models.Tools.CATEGORY_TYPE
+    for item in tools_details:
+        data = {"toolsIcon": str(item.icon), "toolsTitle": item.title,
+                "toolsCate": tools_cate[item.cate - 1], "toolsTag": item.tag,
+                "toolsIntroduction": item.introduction,
+                "toolsNewVersion": item.new_version,
+                "toolsContent": item.content, "toolsLookedNum": item.looked_num,
+                "toolsDownloadNum": item.download_num,
+                #"toolsDownCreateTime": item.create_time,
+                }
+        details.append(data)
+        # print(details)
+    if details:
+        return HttpResponse(json.dumps(details[0]))
+    else:
+        err = {
+            "msg": "没有数据",
+            "state": "err"
+        }
+        return HttpResponse(json.dumps(err))
+    pass
